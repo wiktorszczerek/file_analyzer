dec6248000dfe5f86145510ebbeb4ea4214d9150
wiktorszczerek <wiktor.szczerek@protonmail.com>
2020-03-29 19:28:39 +0200

    test

---
+++ src/com/example/analyzer/Constants.java
@@ -0,0 +1,10 @@
+package com.example.analyzer;
+
+public class Constants {
+    private Constants()
+    {
+        throw new AssertionError();
+    }
+
+    public static final String MSG_UNKNOWN_TYPE = "Unknown file type";
+}
---
+++ src/com/example/analyzer/FileAnalyzer.java
@@ -0,0 +1,18 @@
+package com.example.analyzer;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class FileAnalyzer {
+    public static void main(String[] args)
+    {
+        if(args.length!=3)
+        {
+            System.out.println("Usage: java FileAnalyzer file.sth \"%TOFIND\" \"To print if success\"");
+        }
+        else
+        {
+
+        }
+    }
+}
