85009305477da4e3f1bfeee21278f9620ffebf94
Wiktor Jan Szczerek <w.szczerek@stud.elka.pw.edu.pl>
2020-03-29 20:54:16 +0200

    Dirty method, reads all the bytes and stores them in an array.

--- src/com/example/analyzer/FileAnalyzer.java
+++ src/com/example/analyzer/FileAnalyzer.java
@@ -2,6 +2,7 @@ package com.example.analyzer;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileAnalyzer {
     public static void main(String[] args)
@@ -12,7 +13,34 @@ public class FileAnalyzer {
         }
         else
         {
-
+            try
+            {
+                byte[] byteArray = Files.readAllBytes(Paths.get(args[0]));
+                if(checkIfPatternOccurs(byteArray,args[1].getBytes()))
+                {
+                    System.out.println(args[2]);
+                }
+                else System.out.println("This is not a file you're looking for");
+            }
+            catch(IOException e)
+            {
+                System.out.println("Cannot read data. File may not exist");
+            }
         }
+    }
+
+    public static boolean checkIfPatternOccurs(byte[] arr,byte[] pattern)
+    {
+        int counter = 0;
+        for(byte x:arr)
+        {
+            if(x == pattern[counter])
+            {
+                counter++;
+            }
+            else counter = 0;
+            if(counter == pattern.length) break;
+        }
+        return counter == pattern.length;
     }
 }
